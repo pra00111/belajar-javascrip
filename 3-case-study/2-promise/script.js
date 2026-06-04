7ae6c97ceea9278fc2cd9ccdf055f9d1ce9ecf13
pra0011 <praboyo.1998@gmail.com>
2026-06-04 15:34:44 +0700

    feat: update settings to enable AI features and add async/await and fetch examples

--- 3-case-study/2-promise/script.js
+++ 3-case-study/2-promise/script.js
@@ -1,9 +1,9 @@
 // 1. Simple Promise 1
-// const myProm = new Promise((resolve, reject) => {
-//   resolve('resolved');
-// });
+const myProm = new Promise((resolve, reject) => {
+  resolve("resolved");
+});
 
-// console.log(myProm);
+console.log(myProm);
 
 // 2. Simple Promise 2
 // const janji = new Promise((resolve, reject) => {
@@ -27,39 +27,39 @@
 //   });
 
 // 3. Mengubah callback hell menjadi promise
-function bacaFilePromise(namaFile) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const data = undefined;
-      if (!data) {
-        const err = new Error("File Error!");
-        reject(err);
-      } else {
-        console.log(`Data dari file ${namaFile} dibaca.`);
-        resolve(data);
-      }
-    }, 1000);
-  });
-}
+// function bacaFilePromise(namaFile) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const data = undefined;
+//       if (!data) {
+//         const err = new Error("File Error!");
+//         reject(err);
+//       } else {
+//         console.log(`Data dari file ${namaFile} dibaca.`);
+//         resolve(data);
+//       }
+//     }, 1000);
+//   });
+// }
 
-function prosesDataPromise(data) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const hasil = data.toUpperCase();
-      console.log("Data diproses:", hasil);
-      resolve(hasil);
-    }, 1000);
-  });
-}
+// function prosesDataPromise(data) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       const hasil = data.toUpperCase();
+//       console.log("Data diproses:", hasil);
+//       resolve(hasil);
+//     }, 1000);
+//   });
+// }
 
-function tampilkanHasilPromise(hasil) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log("Hasil pemrosesan:", hasil);
-      resolve();
-    }, 1000);
-  });
-}
+// function tampilkanHasilPromise(hasil) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log("Hasil pemrosesan:", hasil);
+//       resolve();
+//     }, 1000);
+//   });
+// }
 
 // // Memanggil fungsi-fungsi dengan Promise
 // bacaFilePromise('data.txt')
